9b7d54ee4864ab7e309414662bb16a11bea0a285
Stephen Call <stephen@calls4.com>
2017-07-12 10:14:26 -0400

    First commit for personal site

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import tachyons from 'tachyons';
+import emoji from 'node-emoji'
+
+class App extends React.Component {
+  render() {
+    const globeEmoji = emoji.get('earth_americas');
+    const fireEmoji = emoji.get('fire')
+    return (
+      <div className='mh4 mv4'>
+        <h1 className='green mb2 tc'>
+          {globeEmoji} Stujah {globeEmoji}
+        </h1>
+      </div>
+    );
+  }
+}
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
