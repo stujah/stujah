448646cccd23aa7aae5866142871dda604fa6b30
Stephen Call <stephen@calls4.com>
2017-07-13 18:03:03 -0400

    Change background

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import tachyons from 'tachyons';
+import 'tachyons';
 import emoji from 'node-emoji'
 
 class App extends React.Component {
   render() {
-    const globeEmoji = emoji.get('earth_americas');
-    const fireEmoji = emoji.get('fire')
     return (
       <div className='mh4 mv4'>
-        <h1 className='green mb2 tc'>
-          {globeEmoji} Stujah {globeEmoji}
+        <h1 className='gray mb2'>
+          Stephen Call
         </h1>
-        <div className='green tc'>
-          The Fellini of Code / The Duke of Kooks / The Buddha of Stu's
-        </div>
-        <div className='green tc mt7 f-6'>
-          UNDER CONSTRUCTION
+        <div className='gray mb2'>
+          Director, Software Development @ MarketMonitors
         </div>
       </div>
     );
